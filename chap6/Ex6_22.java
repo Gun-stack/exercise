eba1804ec55b15ed389323b1b59b23759ae5638f
Gun-stack <fkdlfo@naver.com>
2023-08-22 18:05:12 +0900

    0822 chap6

---
+++ chap6/Ex6_22.java
@@ -0,0 +1,26 @@
+
+public class Ex6_22 {
+	static boolean isNumber(String str) {
+		if(str==null||str.length()==0) {
+			return false;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			if (!(str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * (1) isNumber메서드를 작성하시오.
+	 */
+	public static void main(String[] args) {
+		String str = "123";
+		System.out.println(str + "는 숫자입니까? " + isNumber(str));
+		str = "1234o";
+		System.out.println(str + "는 숫자입니까? " + isNumber(str));
+		str = "";
+		System.out.println(str + "는 숫자입니까? " + isNumber(str));
+	}
+}
